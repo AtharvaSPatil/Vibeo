59267faf33b7ea649e3af581f83172aafc332f12
AtharvaSPatil <121273696+AtharvaSPatil@users.noreply.github.com>
2025-07-06 23:51:19 +0530

    Added utilities

--- src/index.js
+++ src/index.js
@@ -5,4 +5,10 @@ dotenv.config({
     path: './.env'
 })
 
-connectDB()
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`The server is live on port: ${process.env.PORT}`)
+    })
+}).catch((err) => {
+    console.log("MongoDB connect fail", err)
+})
