a2ce5a065d1f8c65cd19a850ed9e0aa8bb1ceb4c
AtharvaSPatil <121273696+AtharvaSPatil@users.noreply.github.com>
2025-07-13 23:27:53 +0530

    created test router and controller

--- src/app.js
+++ src/app.js
@@ -14,4 +14,10 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// Routes
+
+import { userRouter } from "./routes/index.js";
+
+app.use("/api/v1/user", userRouter);
+
 export { app };
