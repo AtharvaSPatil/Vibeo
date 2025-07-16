65655b8ef57ff2c19838cf3b14cd2491484b4f86
AtharvaSPatil <121273696+AtharvaSPatil@users.noreply.github.com>
2025-07-16 15:33:50 +0530

    Typo

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema({
         type: String, //* Cloudinary
         required: [true, "avatar is required"],
     },
-    comerImage: {
+    coverImage: {
         type: String, //* Cloudinary
     },
     watchHistory: [
